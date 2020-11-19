7542b93e4829c61e072f18e55904f92e385aa010
spencerwilson123321 <swilson176@my.bcit.ca>
2020-11-19 12:16:52 -0800

    created a basic CNN on the mnist handwritten digit dataset so you guys can see how it works

---
+++ scripts/index.py
@@ -0,0 +1,44 @@
+import numpy as np
+import mnist
+from tensorflow import keras
+from tensorflow.keras.models import Sequential
+from tensorflow.python.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+from tensorflow.python.keras.utils.np_utils import to_categorical
+
+train_images = mnist.train_images()
+train_labels = mnist.train_labels()
+
+test_images = mnist.test_images()
+test_labels = mnist.test_labels()
+
+train_images = (train_images / 255) - 0.5
+test_images = (test_images / 255) - 0.5
+
+
+train_images = np.expand_dims(train_images, axis=3)
+test_images = np.expand_dims(test_images, axis=3)
+
+num_filters = 8
+filter_size = 3
+pool_size = 2   
+
+model = Sequential([
+    Conv2D(num_filters, filter_size, input_shape=(28, 28, 1)),
+    MaxPooling2D(pool_size=pool_size),
+    Flatten(),
+    Dense(10, activation='softmax'),
+])
+
+model.compile(
+  'adam',
+  loss='categorical_crossentropy',
+  metrics=['accuracy'],
+)
+
+model.fit(
+  train_images,
+  to_categorical(train_labels),
+  epochs=3,
+  validation_data=(test_images, to_categorical(test_labels)),
+)
+
