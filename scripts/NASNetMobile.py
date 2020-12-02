6681efb692f6f115e20e93592dbca654f8aa6586
spencerwilson123321 <swilson176@my.bcit.ca>
2020-12-01 21:05:57 -0800

    added other models

--- scripts/NASNetMobile.py
+++ scripts/NASNetMobile.py
@@ -5,11 +5,10 @@ from tensorflow.python.keras.models import Sequential
 from tensorflow.python.keras.preprocessing import image
 from tensorflow.keras.preprocessing import image_dataset_from_directory
 from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.applications import NASNetMobile
 
 
 # validation_path = '..\\assets\\validation\\'
-
-SAVE_MODEL = False
 
 training_path = '..\\assets\\training\\'
 testing_path ='..\\assets\\testing\\'
@@ -24,7 +23,7 @@ idg = ImageDataGenerator()
 train_data = image_dataset_from_directory(
     training_path,
     labels='inferred',
-    color_mode='grayscale',
+    color_mode='rgb',
     image_size=(128, 128),
     label_mode='categorical',
     shuffle=True,
@@ -36,7 +35,7 @@ train_data = image_dataset_from_directory(
 validation_data = image_dataset_from_directory(
     training_path,
     labels='inferred',
-    color_mode='grayscale',
+    color_mode='rgb',
     image_size=(128, 128),
     label_mode='categorical',
     shuffle=True,
@@ -48,7 +47,7 @@ validation_data = image_dataset_from_directory(
 test_data = image_dataset_from_directory(
     testing_path,
     labels='inferred',
-    color_mode='grayscale',
+    color_mode='rgb',
     image_size=(128, 128),
     label_mode='categorical',
     shuffle=False
@@ -56,19 +55,7 @@ test_data = image_dataset_from_directory(
 
 num_classes = 3
 
-model = Sequential()
-model.add(Conv2D(filters=16, kernel_size=(3,3),activation='relu', input_shape=(128,128,1)))
-model.add(Conv2D(filters=32,kernel_size=(3,3),activation='relu'))
-model.add(MaxPooling2D(2,2))
-model.add(Conv2D(filters=80,kernel_size=(3,3),activation='relu'))
-model.add(MaxPooling2D(2,2))
-model.add(Conv2D(filters=100,kernel_size=(3,3),activation='relu'))
-model.add(MaxPooling2D(2,2))
-model.add(Flatten())
-model.add(Dense(100, activation='relu'))
-model.add(Dense(num_classes, activation='softmax'))
-
-model.summary()
+model = NASNetMobile(input_tensor=Input(shape=(128,128,3)), classes=3, weights=None)
 
 # Keras Metrics
 metrics = tf.keras.metrics
@@ -84,28 +71,5 @@ model.compile(optimizer='adam',
 
 # fit the model to training data
 history = model.fit(train_data, epochs=10, verbose=1)
-
-if SAVE_MODEL:
-    model.save('mymodel')
-
 loss, acc, precision, recall = model.evaluate(test_data, verbose=1)
 print('Test Set: Loss = %.3f || Accuracy = %.3f || Precision = %.3f || Recall = %.3f' % (loss, acc, precision, recall))
-
-
-# plt.plot(history.history['accuracy'])
-# plt.plot(history.history['val_accuracy'])
-# plt.title('model accuracy')
-# plt.ylabel('accuracy')
-# plt.xlabel('epoch')
-# plt.legend(['train', 'test'], loc='upper left')
-# plt.show()
-#
-# # summarize history for loss
-# plt.plot(history.history['loss'])
-# plt.plot(history.history['val_loss'])
-# plt.title('model loss')
-# plt.ylabel('loss')
-# plt.xlabel('epoch')
-# plt.legend(['train', 'test'], loc='upper left')
-# plt.show()
-
