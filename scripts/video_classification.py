0c40d047df7dd2bb5048bc60d78227c1e7898502
dhruvparekh01 <dhruvparekh01@gmail.com>
2020-12-02 16:22:58 -0800

    Added comments Print class labels instead of numbers

--- scripts/video_classification.py
+++ scripts/video_classification.py
@@ -8,21 +8,24 @@ from skimage.transform import resize
 cap = cv2.VideoCapture(0)
 
 classifier = keras.models.load_model('./mymodel')
-
-# THESE MAY BE IN THE WRONG ORDER
 classes = ['Correct mask', 'Improper mask', 'Without mask']
 
 while True:
     # Read the frame
     _, img = cap.read()
+    # Convert to grayscale
     test_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # Resize to 128x128. We dont want to normalize so set preserve_range=True
     test_image = resize(test_image, (128, 128), preserve_range=True)
     test_image = image.img_to_array(test_image)
     test_image = np.expand_dims(test_image, axis=0)
+    # classifier.predict returns an array of probabilities. Pick the highest probable class
     result = np.argmax(classifier.predict(test_image), axis=-1)
+
+    # Print the class label
     print(classes[result[0]])
 
-    # # Display
+    # Display video
     cv2.imshow('img', img)
 
     # Stop if escape key is pressed
